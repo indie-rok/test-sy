11833be38812dff61a7aedcc67567023a21badb0
Emmanuel Orozco <emmanuelorozco@Emmanuels-MacBook-Pro.local>
2020-12-08 17:23:39 +0100

    added UI for table

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,60 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState } from "react";
+import Table from "../components/Table/index.tsx";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
 
 export default function Home() {
+  const [bankFee, setBankFee] = useState("2.9");
+
+  const getProfit = () => {
+    console.log(bankFee);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Profit calculator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Container>
+        <Row>
+          <Col>
+            <img
+              className="d-block mx-auto mt-2"
+              src="https://dummyimage.com/100x50"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1 className="text-center">Profit calculator</h1>
+          </Col>
+        </Row>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <Row>
+          <Col>
+            <Form.Control
+              type="text"
+              value={bankFee}
+              onChange={(e) => {
+                setBankFee(e.target.value);
+              }}
+            />
+          </Col>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <Col>
+            <Button onClick={getProfit}>Get profit</Button>
+          </Col>
+        </Row>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <Row>
+          <Col>
+            <Table />
+          </Col>
+        </Row>
+      </Container>
     </div>
-  )
+  );
 }
